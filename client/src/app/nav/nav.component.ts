a17e52f7f02d4636af6a816515fce9368e8000d8
Dean McCoy <dean92@gmail.com>
2021-11-28 14:24:55 -0600

    Added Register and Cancel.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.ts';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,9 +10,8 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean;
 
-  constructor(private accountService: AccountService) {}
+  constructor(public accountService: AccountService) {}
 
   ngOnInit(): void {}
 
@@ -18,7 +19,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(
       response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error => {
         console.log(error);
@@ -27,6 +27,6 @@ export class NavComponent implements OnInit {
   }
 
   public logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
