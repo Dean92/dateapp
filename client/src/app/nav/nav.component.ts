8938717a029c11f496a17ddb74d73c583ba73287
Dean McCoy <dean92@gmail.com>
2021-12-09 00:26:10 -0600

    Added toastr, changed bootstrap theme. Configured Route Guard.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.ts';
 import { AccountService } from '../_services/account.service';
@@ -11,22 +13,24 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
 
   ngOnInit(): void {}
 
   public login() {
     this.accountService.login(this.model).subscribe(
       response => {
-        console.log(response);
+        this.router.navigateByUrl('/members/');
       },
       error => {
         console.log(error);
+        this.toastr.error(error.error);
       }
     );
   }
 
   public logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
