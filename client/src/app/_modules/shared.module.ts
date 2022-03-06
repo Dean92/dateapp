b66bc9313b46ce056deeede434ee39c03f6a24ad
Dean McCoy <dean92@gmail.com>
2022-03-06 15:10:09 -0600

    Image gallery for Members. Member detail.

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, BsDropdownModule.forRoot(), ToastrModule.forRoot({ positionClass: 'toast-bottom-right' })],
-  exports: [BsDropdownModule, ToastrModule],
+  imports: [CommonModule, BsDropdownModule.forRoot(), ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }), TabsModule.forRoot(), NgxGalleryModule],
+  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule],
 })
 export class SharedModule {}
