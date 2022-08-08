17f9f4cbec7c4ba04ea83387ef1f506c35da011d
Dean McCoy <dean92@gmail.com>
2022-08-07 23:30:34 -0500

    Added photo editor. Delete Method.

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -4,10 +4,11 @@ import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, BsDropdownModule.forRoot(), ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }), TabsModule.forRoot(), NgxGalleryModule],
-  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule],
+  imports: [CommonModule, BsDropdownModule.forRoot(), ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }), TabsModule.forRoot(), NgxGalleryModule, FileUploadModule],
+  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule, FileUploadModule],
 })
 export class SharedModule {}
