55d6bea8af28d50253d575884681329ae994af83
Dean McCoy <dean92@gmail.com>
2022-10-16 15:14:26 -0500

    Work on form control. Validation. Input

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
+    TextInputComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, SharedModule, NgxSpinnerModule],
   providers: [
