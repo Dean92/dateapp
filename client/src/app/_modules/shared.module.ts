5ad7f95724bad86796af26c7346871b689e08434
Dean McCoy <dean92@gmail.com>
2022-11-05 15:55:08 -0500

    register form. Calendar and various inputs.

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -5,10 +5,11 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, BsDropdownModule.forRoot(), ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }), TabsModule.forRoot(), NgxGalleryModule, FileUploadModule],
-  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule, FileUploadModule],
+  imports: [CommonModule, BsDropdownModule.forRoot(), ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }), TabsModule.forRoot(), NgxGalleryModule, FileUploadModule, BsDatepickerModule.forRoot(),],
+  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule, FileUploadModule, BsDatepickerModule],
 })
 export class SharedModule {}
